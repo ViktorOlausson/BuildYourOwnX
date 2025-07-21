28a7c77636b7c934c4766d0ac8e9817db2c474b8
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2025-07-21 20:39:45 +0200

    welcome message

--- TextEditorInC/main.c
+++ TextEditorInC/main.c
@@ -9,6 +9,8 @@
 #include <sys/ioctl.h>
 
 /*** defines ***/
+#define EDITOR_VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
@@ -134,8 +136,23 @@ void abFree(struct abuf *ab) {
 
 void editorDrawRows(struct abuf *ab) {
     for (int y = 0; y < E.screenrows; y++) {
-        abAppend(ab, "~", 1);
+        if (y == E.screenrows / 3) {
+            char welcome[80];
+            int welcomeLen = snprintf(welcome, sizeof(welcome),
+                "Text Editor -- version %s", EDITOR_VERSION);
+            if (welcomeLen > E.screencols) welcomeLen = E.screencols;
+            int padding = (E.screencols - welcomeLen) / 2;
+            if (padding) {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while (padding--) abAppend(ab, " ", 1);
+            abAppend(ab, welcome, welcomeLen);
+        }else {
+            abAppend(ab, "~", 1);
+        }
 
+        abAppend(ab, "\x1b[K", 3);
         if (y < E.screenrows - 1) {
             abAppend(ab, "\r\n", 2);
         }
@@ -145,12 +162,14 @@ void editorDrawRows(struct abuf *ab) {
 void editorRefreshScreen() {
     struct abuf ab = ABUF_INIT;
 
-    abAppend(&ab, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[?25l", 6);
+    //abAppend(&ab, "\x1b[2J", 4);
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
 
     abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25h", 6);
 
     write(STDOUT_FILENO, ab.buf, ab.len);
     abFree(&ab);
