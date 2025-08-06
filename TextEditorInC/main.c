189844cb2d1e2025af7116309542baa85ae182ef
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2025-08-06 15:19:35 +0200

    some more seach

--- TextEditorInC/main.c
+++ TextEditorInC/main.c
@@ -432,14 +432,40 @@ void editorSave() {
 /*** find ***/
 
 void editorFindCallback(char *query, int key) {
-    if (key == '\r' || key == '\x1b') return;
+    static int lastMatch = -1;
+    static int direction = 1;
 
+    if (key == '\r' || key == '\x1b') {
+        lastMatch = -1;
+        direction = 1;
+        return;
+    }else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+        direction = 1;
+    }else if (key == ARROW_LEFT || key == ARROW_UP) {
+        direction = -1;
+    }else {
+        lastMatch = -1;
+        direction = 1;
+    }
+
+
+    if (lastMatch == -1) {
+        direction = 1;
+    }
+    int current = lastMatch;
     int i;
     for (i = 0; i < E.nrRows; i++) {
-        erow *row = &E.row[i];
+        current += direction;
+        if (current == -1) {
+            current = E.nrRows - 1;
+        }else if (current == E.nrRows) {
+            current = 0;
+        }
+        erow *row = &E.row[current];
         char *match = strstr(row->render, query);
         if (match) {
-            E.cursorY = i;
+            lastMatch = current;
+            E.cursorY = current;
             E.cursorX = editorRowRxToCx(row, match - row->render);
             E.rowOff = E.nrRows;
             break;
@@ -448,11 +474,19 @@ void editorFindCallback(char *query, int key) {
 }
 
 void editorFind() {
-    
+    int savedCursorX = E.cursorX;
+    int savedCursorY = E.cursorY;
+    int savedColOff = E.colOff;
+    int savedRowOff = E.rowOff;
 
-    char *query = editorPrompt("Find: %s (ESC to cancel)", editorFindCallback);
+    char *query = editorPrompt("Find: %s (use ESC/Arrows/Enter)", editorFindCallback);
     if (query) {
         free(query);
+    }else {
+        E.cursorX = savedCursorX;
+        E.cursorY = savedCursorY;
+        E.colOff = savedColOff;
+        E.rowOff = savedRowOff;
     }
 }
 
