9d6bbb08c2db9e3c9f83646a152a4f8824ca292d
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2025-08-18 14:45:37 +0200

    made PS script for building and pushing texteditorinc docker

---
+++ dockerBuildPushTexteditorinc.ps1
@@ -0,0 +1,15 @@
+Set-Location "C:\Users\vikto\source\git\BuildYourOwnX\TextEditorInC"
+
+docker build -t viktorolausson/texteditorinc .
+
+if($LASTEXITCODE -ne 0){
+    Write-Error "Docker Build failed, stopping script"
+}
+
+docker push viktorolausson/texteditorinc
+
+if($LASTEXITCODE -ne 0){
+    Write-Error "Docker Push failed"
+}
+
+Write-Output "Docker build and push sucessfully"
