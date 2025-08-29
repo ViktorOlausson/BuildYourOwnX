d313cda4a1461e899e6bddf2f4054c7ea8abb62d
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2025-08-29 19:32:20 +0200

    comments

--- NodeJsWebServer/server.js
+++ NodeJsWebServer/server.js
@@ -12,7 +12,7 @@ function createWebServer(reqHandler){
             let buf;
             let reqHead;
             while(true){
-                //read data from buf
+                //read data from socket
                 buf = socket.read();
                 //stop if no data
                 if(buf === null) break;
@@ -27,12 +27,14 @@ function createWebServer(reqHandler){
                     let remaining = reqBuffer.slice(marker + 4);
                     // The header is everything we read, up to and not including \r\n\r\n
                     reqHead = reqBuffer.slice(0, marker).toString()
-
+                    // This pushes the extra data we read back to the socket's readable stream
                     socket.unshift(remaining)
                     break
                 }
             }
           
+            /* Request-related business */
+            // Start parsing the header
             const reqHeaders = reqHead.split('\r\n')
             const reqLine = reqHeaders.shift().split(' ')
             const headers = reqHeaders.reduce((acc, currentHeader) => {
@@ -43,6 +45,7 @@ function createWebServer(reqHandler){
                 }
             }, {})
 
+            // This object will be sent to the handleRequest callback.
             const request = {
                 method: reqLine[0],
                 url: reqLine[1],
@@ -51,26 +54,31 @@ function createWebServer(reqHandler){
                 socket
             }
 
+
+            /* Response-related business */
+            // Initial values
             let status = 200, statusText = 'OK', headersSent = false, isChunked = false;
 
             const responseHeaders = {
                 server: 'my-special-server-5000'
             }
-
             function setHeader(key, value){
                 responseHeaders[key.toLowerCase()] = value
             }
 
             function sendHeaders(){
+                // Only do this once :)
                 if(!headersSent){
                     headersSent = true
+                    // Add the date header
                     setHeader('date', new Date().toGMTString())
-
+                    // Send the status line
                     socket.write(`HTTP/1.1, ${status} ${statusText}\n\r`)
-
+                    // Send each following header
                     Object.keys(responseHeaders).forEach(headerKey => {
                         socket.write(`${headerKey}: ${responseHeaders[headerKey]}\r\n`)
                     })
+                    // Add the final \r\n that delimits the response headers from body
                     socket.write('\r\n')
                 }
             }
@@ -78,6 +86,7 @@ function createWebServer(reqHandler){
             const response = {
                 write(chunk){
                     if(!headersSent){
+                        // If there's no content-length header, then specify Transfer-Encoding chunked
                         if(!responseHeaders['content-length']){
                             isChunked = true
                             setHeader('transfer-encoding', 'chunked')
@@ -94,7 +103,9 @@ function createWebServer(reqHandler){
                     }
                 }, end(chunk){
                     if(!headersSent){
+                        // We know the full length of the response, let's set it
                         if(!responseHeaders['content-length']){
+                            // Assume that chunk is a buffer, not a string!
                             setHeader('transfer-encoding', chunk? chunk.length : 0)
                         }
                         sendHeaders()
@@ -111,6 +122,7 @@ function createWebServer(reqHandler){
                         socket.end(chunk)
                     }
                 },setHeader, setStatus(newStatus, newStatusText){status = newStatus, statusText = newStatusText},
+                // Convenience method to send JSON through server
                 json(data){
                     if(headersSent){
                         throw new Error('Headers sent, can not proceed to send json')
@@ -122,6 +134,7 @@ function createWebServer(reqHandler){
                     socket.end(json);
                 }
             }
+            // Send the request to the handler!
             reqHandler(request, response)
         })
     }
