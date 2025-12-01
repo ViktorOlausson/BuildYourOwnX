3dfb0a77519ae551375a42ea0d40d7ec737c4a77
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2025-12-01 21:31:53 +0100

    rename varible

--- Scripts/buildMain.ps1
+++ Scripts/buildMain.ps1
@@ -1,6 +1,6 @@
-$pwd = "cd .. && " + "cd TextEditorInC && " + "make test && " + "./test_editor && " + "make clean && " + "make main"
+$command = "cd .. && " + "cd TextEditorInC && " + "make test && " + "./test_editor && " + "make clean && " + "make main"
 
-wsl -d Ubuntu sh -c "$pwd"
+wsl -d Ubuntu sh -c "$command"
 
 if($LASTEXITCODE -ne 0){
     Write-Error "Build or tests failed inside WSL (exit code $LASTEXITCODE)"
